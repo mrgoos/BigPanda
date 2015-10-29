d975d2d24913ae7be7f1ff3cb18c8944233c2a6a
oferm <oferm@infolinks.com>
2015-10-29 09:05:55 +0200

    Better error handling

--- server/index.js
+++ server/index.js
@@ -8,28 +8,37 @@ app.get('/status', function (req, res) {
 	http.get('https://status.github.com/api/status.json', function(response) {
 		handleResponse(response, req, res);		
 	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
+		console.log(e);
+		res.status(500).send('http get error: ' + e);
 	});
 });
 
 app.get('/messages', function (req, res) {
-	http.get('https://status.github.com/api/messages.json', function(response) {
+	http.get('https://status.github.com/api/messages.jsooon', function(response) {
 		handleResponse(response, req, res);	
 	}).on('error', function(e) {
-		console.log("Got error: " + e.message);
+		console.log(e);
+		res.status(500).send('http get error: ' + e);
 	});
 });
 
 function handleResponse(response, req, res){
 	// Continuously update stream with data
         var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
-        response.on('end', function() {
-            // Data reception is done, send it to client
-            res.send(body);
-        });
+		if(response.statusCode == 200){
+			response.on('data', function(d) {
+				body += d;
+			});
+			response.on('end', function() {
+				// Data reception is done, send it to client
+				
+				res.send(body);
+			});
+		}
+		else{
+			//for any http request error, send the status message
+			res.status(response.statusCode).send('Server error. Status message: ' + response.statusMessage);
+		}  
 }
 
 app.get('/', function (req, res) {
